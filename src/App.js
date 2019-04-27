e289ec22f366ca117107965244af0fe00c26f994
cgycch <1136856486@qq.com>
2019-04-27 22:28:23 +0800

    user

--- src/App.js
+++ src/App.js
@@ -14,8 +14,7 @@ class App extends Component {
           <a
             className="App-link"
             href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+            target="_blank" rel="noopener noreferrer"
           >
             Learn React
           </a>
