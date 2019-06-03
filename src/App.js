bc867cb61e0e753693ebe62c2944a4eb23ba3f8a
cgycch <1136856486@qq.com>
2019-06-03 23:02:27 +0800

    HELLO USERNAME

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
+import { getUserInfos } from './ui/actions/common/user'
 import BasicNavExample from './ui/components/BasicNavExample'
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.setState({
+      userId: '00001',
+      userName: 'admin'
+    })
+  }
+  componentDidMount = () => {
+    console.log('componentDidMount...')
+    getUserInfos().then(res => {
+      const { userId, userName } = res
+      this.setState({ userId, userName })
+    }).catch(err => {
+      console.log('error on get user info', err)
+    })
+  }
+
   render() {
+    let userName = 'xxx'
+    if (this.state != null) {
+      userName = this.state.userName
+    }
     return (
       <div className="App">
-         <h3>Hello chunhui, you are so handsome!</h3>
-         <BasicNavExample/>
+        <h3>Hello {userName}, you are so handsome!</h3>
+        <BasicNavExample />
       </div>
     );
   }
---
+++ src/ui/actions/common/user.js
@@ -0,0 +1,9 @@
+
+export const getUserInfos =()=>{
+    let userInfo = {
+        userId:'00008',
+        userName:'user abc'
+    }
+    console.log('getUserInfos',new Date())
+    return Promise.resolve(userInfo)
+   } 
